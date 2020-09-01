d7a207a355add14efe4b82e46e9067d315eced65
Marlon Saldanha <marlon.saldanha@acad.pucrs.br>
2020-09-01 20:13:07 -0300

    fix(index.js): adiciona mysql ao  arquivo de configuracao de ambiente

--- server/config/env/index.js
+++ server/config/env/index.js
@@ -1,16 +1,31 @@
 development = {
-    env: "development",
-    apiURL: "http://localhost",
-    apiPort: 8080,
+  env: 'development',
+  apiURL: 'http://localhost',
+  apiPort: 8080,
+
+  sqlite: {
     dataConfig: {
-        dialect: "sqlite",
-        storage: "./database.sqlite3"
-    }
-}
+      dialect: 'sqlite',
+      storage: './database.sqlite3',
+    },
+  },
+  mysql: {
+    dialect: 'mysql',
+    host: 'db-appoio.cpr4q5lamt9g.sa-east-1.rds.amazonaws.com',
+    port: 3306,
+    username: 'admin',
+    password: '12345678',
+    database: 'db-appoio',
+
+    define: {
+      timestamp: true,
+      underscored: true,
+    },
+  },
+};
 
 production = {
-    nomeApi: "appoio-backend",
-}
-
+  nomeApi: 'appoio-backend',
+};
 
 module.exports = development;
