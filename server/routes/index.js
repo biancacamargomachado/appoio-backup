d4f5b520f6c6bcf6e633bc6d56c5c9ac6b1807cc
jojo0severo <33180595+jojo0severo@users.noreply.github.com>
2020-09-18 17:35:02 -0300

    Update index.js

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,11 +1,9 @@
 const express = require('express');
-const sanityCheck = require('./sanityCheck');
 const userRoute = require('./UserRoute');
 const tutorialRoute = require('./TutorialRoute');
 
 const router = express.Router();
 
-router.use('/check', sanityCheck);
 router.use('/user', userRoute);
 router.use('/tutorial', tutorialRoute);
 
