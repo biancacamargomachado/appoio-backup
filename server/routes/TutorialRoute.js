21ee33a98e06a5dafe882c0919af36003ac14be8
jojo0severo <joaovbsevero@gmail.com>
2020-09-17 21:46:23 -0300

    Corrigido erro de conflito entre id e categorias

--- server/routes/TutorialRoute.js
+++ server/routes/TutorialRoute.js
@@ -1,19 +1,20 @@
 const express = require('express');
 const multer = require('multer')
 const tutorialController = require('../controllers/TutorialController');
-var storage = require('../helper/ImageHandler');
+const storage = require('../helper/ImageHandler');
 
 const router = express.Router();
 
-const upload = multer({ storage: storage }).single('file');
+const upload = multer({ storage: storage }).array('images');
+
+//Rota para acessar tutorial por id
+router.route('/id/:id').get(tutorialController.get);
+
+//Rota para acessar tutoriais de todas categorias
+router.route('/categories').get(tutorialController.getAll);
 
 // Rota para registrar um tutorial
 router.route('/registration').post(upload, tutorialController.register);
 
-//Rota para acessar tutoriais de uma categoria específica
-router.route('/category/:category').get(tutorialController.getAll);
-
-//Rota para acessar tutorial por id
-router.route('/:id').get(tutorialController.get);
 
 module.exports = router;
