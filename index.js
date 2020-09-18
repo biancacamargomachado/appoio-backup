f94eb9464e4972906c6bd66f96e08bb26431a8a2
jojo0severo <33180595+jojo0severo@users.noreply.github.com>
2020-09-18 17:36:04 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const app = require('./server/config/express');
 const config = require('./server/config/env');
 
+app.get('/status', (req, res) => res.send({status: 'Ok'}));
+
 app.listen(config.apiPort, () => {
   console.log(
     `API Server started and listening on port ${config.apiPort} (${config.env})`
